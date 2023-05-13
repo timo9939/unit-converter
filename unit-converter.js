76528a62101242aeaaa26f8d4d53d22dcf1e0013
timo9939 <timothylau20199@gmail.com>
2023-05-13 22:26:14 +0100

    Radio button html fixed

---
+++ unit-converter.js
@@ -0,0 +1,11 @@
+let tempUnit=document.getElementById("celsius").value
+
+switch (tempUnit){
+    case celsius:
+        console.log("°C")
+        break;
+        
+    case fahrenheit:
+        console.log("F")
+        break;
+}
